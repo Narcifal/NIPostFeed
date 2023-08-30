3643b1d65c72810f0fbaeca4b160bed3dbb95ae6
Denys Niestierov <nercifal@gmail.com>
2023-08-30 16:18:02 +0300

    base init

---
+++ NIPostListModule/View/NIPostListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NIPostListViewController.swift
+//  NIPostFeed
+//
+//  Created by Denys Niestierov on 30.08.2023.
+//
+
+import UIKit
+
+class NIPostListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
